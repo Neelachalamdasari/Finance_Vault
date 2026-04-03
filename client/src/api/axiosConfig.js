d91a6a9fa99133bee4d366f1974ae0b2de30c64b
Neelachalamdasari <neelachalamdasari33@gmail.com>
2026-04-04 02:40:40 +0530

    Update axiosConfig.js

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const API_BASE =
-  process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+  process.env.REACT_APP_API_URL || 'https://finance-vault-server.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE,
